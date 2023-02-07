de78a078b2caf81c1ee0e64fa0d8b20d0badf375
Vinko Mlacic <vinko@chino.io>
2023-02-07 09:31:22 +0100

    Add logout button and user icon
    Refs: #REC-6

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from .views import *
@@ -21,4 +22,5 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('login', RecipyLoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 
 from accounts.forms import RecipyAuthenticationForm
@@ -7,6 +8,7 @@ from accounts.forms import RecipyAuthenticationForm
 class RecipyLoginView(LoginView):
     template_name = 'accounts/login.html'
     form_class = RecipyAuthenticationForm
+    next_page = reverse_lazy('recipy:index')
     extra_context = {
         'form_title': _('Login'),
         'form_submit_label': _('Login'),
