29f6ece715c2337dfabf840a983f80e4e79caafb
Vinko Mlacic <vinko@chino.io>
2023-08-15 21:14:24 +0200

    Conform to new static file architecture
    Refs: #DEP-6

--- root/settings/django.py
+++ root/settings/django.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_ROOT = str(BASE_DIR / 'static')
-STATIC_URL = '/static/'
+STATIC_URL = '/static/recipy/'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
